2cd9a3829ad91e36b6c3b481b133d37b2f632725
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2023-04-12 12:13:31 -0300

    Fin clase 13

---
+++ archivo.js
@@ -0,0 +1,40 @@
+// Variables:
+// var noUsar
+let paraCambiar = 2;
+const valorFijo = "asas";
+
+// Operaciones:
+paraCambiar = paraCambiar + 3;
+console.log('Resultado:', paraCambiar);
+
+paraCambiar = paraCambiar - 1;
+console.log('Resultado:', paraCambiar);
+
+paraCambiar = paraCambiar * 2;
+console.log('Resultado:', paraCambiar);
+
+paraCambiar = paraCambiar / 4;
+console.log('Resultado:', paraCambiar);
+
+console.log(`El valor actual es ${paraCambiar} (estoy hablando de la variable)`)
+console.log({ paraCambiar }) // 2
+
+paraCambiar = '2';
+
+let pregunta = paraCambiar === 2
+let pregunta2 = paraCambiar == 2
+
+if (pregunta) {
+    console.log('\n\nparaCambiar es igual a dos')
+} else {
+    console.log('\n\nparaCambiar no es para nada igual a dos')
+}
+
+
+if (pregunta) {
+    console.log('\n\nparaCambiar es del mismo tipo que 2')
+} else if (pregunta2) {
+    console.log('\n\nparaCambiar no es del tipo número pero si lo convierto es igual a 2')
+}
+
+// console.log(algo)
